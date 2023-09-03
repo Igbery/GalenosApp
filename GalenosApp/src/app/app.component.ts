6b9caeb2bfa8fad7f954bf9cf8bd058bac951e15
Ignacio Paredes <ig.paredes@duocuc.cl>
2023-09-03 03:13:49 -0300

    Se intentó crear un boton que abra el menu lateral

--- GalenosApp/src/app/app.component.ts
+++ GalenosApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,19 @@ export class AppComponent {
     { title: 'Login', url: './login', icon: 'accessibility' },
     { title: 'Register', url: './register', icon: 'add-circle' }
   ];
-  constructor(public router: Router) {
+  constructor(private menu: MenuController, public router: Router) {
     this.initializeApp();
   }
   initializeApp(){
-    this.router.navigateByUrl('splash')
-  };
+    this.router.navigateByUrl('splash');
+  }
+  ngOnInit(){
+    this.openMenu();
+  }
+  openMenu(){
+    this.menu.open();
+  }
+  closeMenu(){
+    this.menu.close();
+  }
 }
