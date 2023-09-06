13e642c12de49c22d75cbf378b96891328d7d017
Igbery <104919963+Igbery@users.noreply.github.com>
2023-09-06 19:31:31 +0000

    Se condiciona de forma basica la page Restaurar y se conecta con Login y Register

--- GalenosApp/src/app/login/login.page.ts
+++ GalenosApp/src/app/login/login.page.ts
@@ -22,4 +22,8 @@ export class LoginPage implements OnInit {
     this.router.navigateByUrl('/main');
   }
 
+  navigateToRestaurar(){
+    this.router.navigateByUrl('/restaurar')
+  }
+
 }
--- GalenosApp/src/app/restaurar/restaurar.page.ts
+++ GalenosApp/src/app/restaurar/restaurar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-restaurar',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurarPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateToLogin(){
+    this.router.navigateByUrl('/login');
+  }
+
+  navigateToRegister(){
+    this.router.navigateByUrl('/register')
   }
 
 }
