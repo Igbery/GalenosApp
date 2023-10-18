50cd4c81906b1122f837ee747abc79676aff2057
Ignacio Paredes <ig.paredes@duocuc.cl>
2023-10-18 01:37:52 -0300

    Se intenta conectar la BD de firestore

--- GalenosApp/src/environments/environment.prod.ts
+++ GalenosApp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyARDGJR_QiWOtTKOYVUC31Or5J8qGlgudY",
+    authDomain: "galenosapp.firebaseapp.com",
+    projectId: "galenosapp",
+    storageBucket: "galenosapp.appspot.com",
+    messagingSenderId: "536745522201",
+    appId: "1:536745522201:web:31f3a793acd483e0d0a688",
+    measurementId: "G-69R44YY2LV"
+  }
 };
--- GalenosApp/src/environments/environment.ts
+++ GalenosApp/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyARDGJR_QiWOtTKOYVUC31Or5J8qGlgudY",
+    authDomain: "galenosapp.firebaseapp.com",
+    projectId: "galenosapp",
+    storageBucket: "galenosapp.appspot.com",
+    messagingSenderId: "536745522201",
+    appId: "1:536745522201:web:31f3a793acd483e0d0a688",
+    measurementId: "G-69R44YY2LV"
+  }  
 };
 
 /*
